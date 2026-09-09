0d32873f271469c4e303187a086920ac823eee77
canta-9142 <work@floating-gate.com>
2026-09-09 21:11:52 +0900

    fix(nix): provide Git for release script tests

--- flake.nix
+++ flake.nix
@@ -43,6 +43,7 @@
           subPackages = [ "cmd/qshare" ];
           env.CGO_ENABLED = "0";
           ldflags = [ "-X main.version=v${packageVersion}" ];
+          nativeCheckInputs = [ pkgs.git ];
 
           checkPhase = ''
             runHook preCheck
